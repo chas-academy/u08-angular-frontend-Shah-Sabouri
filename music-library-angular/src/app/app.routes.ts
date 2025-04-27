d0554d171ce9bf38cebae2aefbc89fb1b8a9c42d
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-04-27 21:41:59 +0200

    set up initial routing

--- music-library-angular/src/app/app.routes.ts
+++ music-library-angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SongListComponent } from './components/song-list/song-list.component';
+import { SongFormComponent } from './components/song-form/song-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SongListComponent },
+    { path: 'add', component: SongFormComponent},
+    { path: 'edit/:id', component: SongFormComponent },
+];
