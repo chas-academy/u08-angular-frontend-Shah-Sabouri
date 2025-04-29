a7696f81947551b3a271737ebc362b91dbecc09e
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-04-29 19:09:51 +0200

    imported Song model into service, updated methods with relevant info

--- music-library-angular/src/app/services/song.service.ts
+++ music-library-angular/src/app/services/song.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Song } from '../models/song.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,27 +12,27 @@ export class SongService {
   constructor(private http: HttpClient) { }
 
   // HÄMTA ALLA LÅTAR
-  getSongs(id: string): Observable<any> {
-    return this.http.get(`${this.apiURL}`)
+  getSongs(): Observable<Song> {
+    return this.http.get<Song>(`${this.apiURL}`)
   }
 
   // HÄMTA LåT MED ID
-  getSongById(id: string): Observable<any> {
-    return this.http.get(`${this.apiURL}/${id}`)
+  getSongById(id: string): Observable<Song> {
+    return this.http.get<Song>(`${this.apiURL}/${id}`)
   }
 
   // LÄGG TILL LÅT
-  addSong(song: any): Observable<any> {
-    return this.http.post(this.apiURL, song);
+  addSong(song: Song): Observable<Song> {
+    return this.http.post<Song>(this.apiURL, song);
   }
 
   // UPPDATERA LÅT
-  updateSong(id: string, song: any): Observable<any> {
-    return this.http.put(`${this.apiURL}/${id}`, song);
+  updateSong(id: string, song: Song): Observable<Song> {
+    return this.http.put<Song>(`${this.apiURL}/${id}`, song);
   }
 
   // RADERA LÅT
-  deleteSong(id: string): Observable<any> {
-    return this.http.delete(`${this.apiURL}/${id}`);
+  deleteSong(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiURL}/${id}`);
   }
 }
