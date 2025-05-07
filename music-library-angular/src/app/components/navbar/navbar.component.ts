300ce2f7bec4fec4f517df675ff9b79aee840868
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-05-07 17:40:44 +0200

    navbar updated with implementation of drawer function

--- music-library-angular/src/app/components/navbar/navbar.component.ts
+++ music-library-angular/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SongDrawerComponent } from '../../song-drawer/song-drawer.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, SongDrawerComponent],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  showDrawer = false;
 
+  toggleDrawer() {
+    this.showDrawer = !this.showDrawer;
+  }
+
+  closeDrawer() {
+    this.showDrawer = false;
+  }
 }
